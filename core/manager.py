0d1a4a4ed11aee3c5285f21f6274e2d11e12ddf2
D_Art <arthur.dobrynin@gmail.com>
2018-08-11 10:39:24 +0200

    Adding basic model manager functions

--- core/manager.py
+++ core/manager.py
@@ -1,3 +1,29 @@
 
+from claim.handlers import *
+
 class ModelManager():
-    pass
+
+    class ManagerException(Exception):
+        def __init__(self, msg):
+            super().__init__(msg)
+
+    def __init__(self):
+        self._models = []
+        self._default_model = None
+
+    def add_model(self, model):
+        if not issubclass(model, AbstractClaimHandler):
+            raise ManagerException("Model should be a subclass of %s" % AbstractClaimHandler)
+        self._models.append(model)
+
+    def set_default_model(self, model_cl):
+        for mod in self._models:
+            if isinstance(mod, model_cl):
+                self._default_model = mod
+                break
+
+        if not self._default_model:
+            raise ManagerException("Can't set model %s before it is added" % model_cl)
+
+    def process_claims(self, claims):
+        return [self._default_model.classify_claim(c) for c in claims]
